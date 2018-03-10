ea6f605661607ccebecc286dd39a31e801cbe9be
TrevorSands <trevorsands@gmail.com>
2018-03-09 20:14:37 -0500

    Added register method for rule.

--- Aspecter.py
+++ Aspecter.py
@@ -9,17 +9,30 @@ class Aspecter(object):
     # some attribute of the class/object you care about and want to look out for
     typeSignifiers = {}
 
+    # list containing registered rules
+    rules = []
+
     # constructor here
+    def __init__(self):
+        pass
 
     # static methods
     # @staticmethod decorator
 
     # instance methods
+    # @classmethod decorator
 
-    # add to and remove from methodNames methods
-
+    @classmethod
     def addToMethodNames(self, methodObj):
         self.methodNames[methodObj.__name__] = methodObj
 
+    @classmethod
     def removeFromMethodNames(self, name):
         del self.methodNames[name]
+
+    @classmethod
+    def register(self, regex="", inputObjs=(), outputObjs=(),
+                 prefunc=None, postfunc=None):
+        rule = {"regex":regex, "inputs":inputObjs, "outputs":outputObjs,
+                "pre":prefunc, "post":postfunc}
+        self.rules.append(rule)
