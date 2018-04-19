04d616d4dc4c8a75c7bc53d1d9708f7adf10ed6f
FrankCardillo <facardillo91@gmail.com>
2018-04-18 20:50:23 -0400

    working first pass at a metaclass hook aspecter

--- MetaClassAspecter.py
+++ MetaClassAspecter.py
@@ -1,60 +1,35 @@
+def wrapper(func, args):
+    func(*args)
+
 class MetaClassAspecter(type):
-    
-    # user defines a dict of methods to decorate, 
-    # keys are string representations of method names, 
-    # values are a list of pre-functions, post-functions, 
-    # and the method with any arguments they need
-    # EX: {'print': [preFunc('hello'), postFunc('goodBye'), funcToWrap(1, 2, 3)]}
-
-    def __new__(cls, name, bases, dct):
-        if dct['userDct']:
-            for key in dct['userDct']:
-                if dct[key]:
-                    dct[key] = cls.decorate(
-                        dct[key],
-                        dct['userDct'][key][0],
-                        dct['userDct'][key][1],
-                        dct['userDct'][key][2]
-                    )
-                else:
-                    raise Exception(f'method {key} is not in the class dict, can not decorate')
-        return type.__new__(cls, name, bases, dct)
-
-    # can decorate any class/instance methods
-
-    def decorate(cls, funcToWrap, wrapArgs, preFunc=None, postFunc=None):
-        if preFunc and callable(preFunc):
-            preFunc
-
-        retVal = funcToWrap(wrapArgs)
-
-        if postFunc and callable(postFunc):
-            postFunc
-
-        return retVal
-
-class Test(object):
-    __metaclass__ = MetaClassAspecter
-
-    userDct = {
-            'testFunction': [
-                ['I am the test function'],
-                lambda self: print('Hello World'),
-                lambda self: print('Goodbye World')
-            ]
-        }
-
-    def testFunction(self, arg1):
-        print(arg1)
-
-if __name__ == '__main__':
-    ourTest = Test()
-    ourTest.testFunction('I am the test function')
+    def __new__ (cls, name, bases, dct, **aspectOptions):
+        try:
+            if aspectOptions['onNew']:
+                wrapper(aspectOptions['aspect'], aspectOptions['arguments'])
+        except AttributeError:
+            print("Please be sure to add these class parameters: aspect, arguments, onInit, onNew")
+        return type . __new__ ( cls , name , bases , dct )
+    def __init__ (cls, name, bases, dct, **aspectOptions):
+        try:
+            if aspectOptions['onInit']:
+                wrapper(aspectOptions['aspect'], aspectOptions['arguments'])
+        except AttributeError:
+            print("Please be sure to add these class parameters: aspect, arguments, onInit, onNew")
+        super (MetaClassAspecter, cls).__init__(name, bases, dct)
 
 
 
-# We can't define a generic decorator on the meta class hook.
-# It breaks the user's ability to pass arguments to the function when calling it. 
-# They would have to know the arguments ahead of time and put them in userDict.
-# We'll have to define useful general decorations on the metaclass hook and
-# leave the customized instrumenting to dynamic mutation implementation(s)
+
+def funTime(oneArg, twoArg, threeArg):
+    print("Hello world! I have been meta thinged")
+    print(oneArg)
+    print(twoArg)
+    print(threeArg)
+
+class Foo(metaclass=MetaClassAspecter, aspect=funTime, arguments=["I'm the first", "I'm the second", "I'm the third"], onInit=False, onNew=True):
+    def __init__(self):
+        print("Foo has been initialized")
+
+
+if __name__ == "__main__":
+    myVar = Foo()
